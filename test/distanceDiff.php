c5a026f3a4f4643f70d7d21f01b0fcd6a0fe304a
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-08-01 18:13:12 +0800

    1.新功能:增加testDistance测试

---
+++ test/distanceDiff.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @title distanceDiff
+ * @description
+ * distanceDiff
+ * @email zhangchunsheng423@gmail.com
+ * @date 2014-08-01
+ * @version V1.0
+ * @copyright  Copyright (c) 2010-2014 Luomor Inc. (http://www.luomor.com)
+ */
