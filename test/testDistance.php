de34b4e6675b6d6aca62c7da9b7ebb176b2901e2
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-08-01 11:04:20 +0800

    1.新功能:增加testDistance测试

---
+++ test/testDistance.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @title testDistance
+ * @description
+ * testDistance
+ * @email zhangchunsheng423@gmail.com
+ * @date 2014-08-01
+ * @version V1.0
+ * @copyright  Copyright (c) 2010-2014 Luomor Inc. (http://www.luomor.com)
+ */
+$map = new YCL_Map();
+
+/*println($map->getDistance(array(
+    array('lng' => -73.994529,'lat' => 40.735243),
+    array('lng' => -74.009735,'lat' => 40.705697)
+)));*/
+
+println($map->getDistance(array(
+    array('lng' => 116.481028,'lat' => 39.989643),
+    array('lng' => 114.465302,'lat' => 40.004717)
+)));
