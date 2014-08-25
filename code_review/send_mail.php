675d314a7fae9a80a927b7898de0c7c68bcbc633
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-08-25 18:56:20 +0800

    1.新功能:update smtp user info

--- code_review/send_mail.php
+++ code_review/send_mail.php
@@ -8,10 +8,10 @@
  * @copyright  Copyright (c) 2010-2014 Luomor Inc. (http://www.luomor.com)
  */
 error_reporting(~E_ALL);
-/*$to = "zhangchunsheng@yongche.com";
+/*$to = "zhangchunsheng@didiwuliu.com";
 $subject = "review code";
 $txt = "review code";
-$headers = "From: noreply@yongche.com" . "\r\n";
+$headers = "From: noreply@didiwuliu.com" . "\r\n";
 
 mail($to, $subject, $txt, $headers);*/
 
